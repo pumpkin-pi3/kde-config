67705781e46718d3888e09923971a0c4723fb435
pumpkin-pi3 <mirta.summers.x@gmail.com>
2022-03-07 17:25:48 +0100

    Yes

---
+++ settingup_ohmyzsh_and_p10k.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i -e '/plugins/s/)/ zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's!robbyrussell!powerlevel10k/powerlevel10k!' ~/.zshrc
+sudo fc-cache -f -v
+sudo locale-gen
